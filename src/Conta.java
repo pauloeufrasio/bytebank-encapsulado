a365404662220de07f679d6157282fcca1724b41
paulo <henriquepaulo0606@gmail.com>
2023-03-08 11:42:47 -0300

    Importancia do encapsulamento de atributos

--- src/Conta.java
+++ src/Conta.java
@@ -10,6 +10,34 @@ public class Conta {
 
     }
 
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
     public boolean saca(double valor) {  // metodo 2
         if (this.saldo >= valor) {
             this.saldo -= valor;
